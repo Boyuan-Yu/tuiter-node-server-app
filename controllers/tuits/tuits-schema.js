9e8a048d4250a5f8e28b225f268f803ee107094c
yuboyu <yu.boyu@northeastern.edu>
2022-11-30 13:41:41 -0800

    a9 finished

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,16 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
+    topic:String,
+    image:String,
+    userName:String,
+    handle:String,
+    time:String,
     tuit: String,
     likes: Number,
+    dislikes: Number,
+    replies: Number,
+    retuits: Number,
     liked: Boolean,
+    disliked: Boolean,
 }, {collection: 'tuits'});
 export default schema;
